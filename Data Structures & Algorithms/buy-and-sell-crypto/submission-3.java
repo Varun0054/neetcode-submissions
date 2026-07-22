2cbc01e2e792bb8835244b31c430fa94e71b14c5
Varun Bhagwat <varunbhagwat948@gmail.com>
2026-07-22 17:43:32 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 0) return 0;
+        int bp = prices[0];
+        int pro = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < bp) {
+                bp = prices[i];
+            } else {
+                int cp = prices[i] - bp;
+                pro = Math.max(pro, cp);
+            }
+        }
+        return pro;
+    }
+}
