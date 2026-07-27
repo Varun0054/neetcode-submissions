55cd00c3100197848a412b8071e2ec49a8e89bce
Varun Bhagwat <varunbhagwat948@gmail.com>
2026-07-27 20:08:45 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,33 @@
+
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        // Push onto minStack if it's empty OR the new val is <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        // Use .equals() or assign both to primitive int to prevent Integer object reference comparison bugs
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
